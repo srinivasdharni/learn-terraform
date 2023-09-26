8c75455b1b995ea14cc9d99108917a3f39745e87
India <India@India-PC>
2023-09-26 22:52:46 +0530

    commited

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@
 	  tags = {
 	    Name = "tf-frontend"
 	  }
-	
 	}
 	
 	resource "aws_route53_record" "frontend" {
@@ -22,7 +21,7 @@
 	
 	  provisioner "local-exec" {
 	    command = <<EOF
-	cd /home/centos/Infra-Ansible
+	cd /home/centos/infra-ansible
 	git pull
 	sleep 60
 	ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
@@ -52,7 +51,7 @@
 	  depends_on = [aws_route53_record.backend]
 	  provisioner "local-exec" {
 	    command = <<EOF
-	cd /home/centos/Infra-Ansible
+	cd /home/centos/infra-ansible
 	git pull
 	sleep 60
 	ansible-playbook -i ${aws_instance.backend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
@@ -83,7 +82,7 @@
 	  depends_on = [aws_route53_record.mysql]
 	  provisioner "local-exec" {
 	    command = <<EOF
-	cd /home/centos/Infra-Ansible
+	cd /home/centos/infra-ansible
 	git pull
 	sleep 60
 	ansible-playbook -i ${aws_instance.mysql.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
